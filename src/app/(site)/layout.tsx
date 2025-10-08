009284d22c2301e9c6d1358fa84e6bfd1aa836bb
grinvsevolod1-ctrl <grin.vsevolod1@gmail.com>
2025-10-08 12:54:46 +0300

    Update layout.tsx

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,20 +1,23 @@
 "use client";
+
 import { useState, useEffect } from "react";
 import "../css/euclid-circular-a-font.css";
 import "../css/style.css";
+
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import ScrollToTop from "@/components/Common/ScrollToTop";
+import PreLoader from "@/components/Common/PreLoader";
+import QuickViewModal from "@/components/Common/QuickViewModal";
+import CartSidebarModal from "@/components/Common/CartSidebarModal";
+import PreviewSliderModal from "@/components/Common/PreviewSlider";
 
 import { ModalProvider } from "../context/QuickViewModalContext";
 import { CartModalProvider } from "../context/CartSidebarModalContext";
 import { ReduxProvider } from "@/redux/provider";
-import QuickViewModal from "@/components/Common/QuickViewModal";
-import CartSidebarModal from "@/components/Common/CartSidebarModal";
 import { PreviewSliderProvider } from "../context/PreviewSliderContext";
-import PreviewSliderModal from "@/components/Common/PreviewSlider";
 
-import ScrollToTop from "@/components/Common/ScrollToTop";
-import PreLoader from "@/components/Common/PreLoader";
+import { SessionProvider } from "next-auth/react";
 
 export default function RootLayout({
   children,
@@ -24,23 +27,23 @@ export default function RootLayout({
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 1000);
+    const timer = setTimeout(() => setLoading(false), 1000);
+    return () => clearTimeout(timer);
   }, []);
 
   return (
-    <html lang="en" suppressHydrationWarning={true}>
+    <html lang="en" suppressHydrationWarning>
       <body>
         {loading ? (
           <PreLoader />
         ) : (
-          <>
+          <SessionProvider>
             <ReduxProvider>
               <CartModalProvider>
                 <ModalProvider>
                   <PreviewSliderProvider>
                     <Header />
                     {children}
-
                     <QuickViewModal />
                     <CartSidebarModal />
                     <PreviewSliderModal />
@@ -50,7 +53,7 @@ export default function RootLayout({
             </ReduxProvider>
             <ScrollToTop />
             <Footer />
-          </>
+          </SessionProvider>
         )}
       </body>
     </html>
